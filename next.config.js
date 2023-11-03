b99dc0ab7e1a8b76f3ac80a3ef7b5ae12994b4b2
pitambersingh729 <pitambersingh729@gmail.com>
2023-11-04 03:04:15 +0530

    2ndpush

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode = true,
+  swcMinify = false,
+  trailingSlash = true,
+}
 
 module.exports = nextConfig
